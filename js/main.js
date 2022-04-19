2c995ced0ad4fdb9900e95c183b6a031ef6bb738
Dmitrii <you@example.com>
2022-04-19 19:08:50 +0700

    adaptive

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ $(function () {
       prevArrow: '<img class="arrows arrow-top" src="images/arrow-top.svg">',
       nextArrow: '<img class="arrows arrow-bottom" src="images/arrow-bottom.svg">',
       fade: true,
+      responsive: [{
+         breakpoint: 361,
+         settings: {
+            dots: false,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
+         }
+      }]
    });
 
    $('.products__name').slick({
@@ -15,6 +24,26 @@ $(function () {
       prevArrow: '<button type="button"><img class="prod-arrows prod-arrow-top" src="images/prod-arrow-top.svg"></button>',
       nextArrow: '<button type="button"><img class="prod-arrows prod-arrow-bottom" src="images/prod-arrow-bottom.svg"></button>',
       centerMode: true,
+      responsive: [
+         {
+            breakpoint: 969,
+            settings: {
+               vertical: false,
+               slidesToShow: 3,
+               arrows: false,
+               dots: true,
+            }
+         },
+         {
+            breakpoint: 769,
+            settings: {
+               vertical: false,
+               slidesToShow: 1,
+               arrows: false,
+               dots: true,
+            }
+         },
+      ]
    });
    $('.products__content').slick({
       asNavFor: '.products__name',
@@ -23,6 +52,9 @@ $(function () {
       fade: true,
       arrows: false,
       // infinite: true,
+   });
 
+   $('.menu__btn').on('click', function () {
+      $('.header__menu-list').toggleClass('header__menu-list--active');
    });
 });
